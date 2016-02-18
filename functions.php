918fee7548a8659d3bd0c39d8038dc10599580ce
Raphaël Tsingos <contact@mooon-web.com>
2016-02-18 17:10:06 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -17,3 +17,21 @@ function getTimeZoneStamp($lat, $long){
   $timeZoneIs = $response['timeZoneId'];
   return array($timeStamp, $timeZoneIs);
 }
+
+function dateTimeDestStamp($dateTime = '', $adresse = '', $codePostal = '', $localite = '', $province = '', $pays = ''){
+	$adresseGPS = $adresse . ' ' . $codePostal . ' ' . $localite . ' ' . $province . ' ' . $pays;
+	$gps = getGPS($adresseGPS);
+	$latitude = $gps[0];
+	$longitude = $gps[1];
+	$timeZone = getTimeZoneStamp($latitude, $longitude);
+	$timeZoneStamp = $timeZone[0];
+	$fuseau = $timeZone[1];
+	// $dateTime format = dd/mm/yyyy hh:mm (ex : 12/05/2020 12:15)
+	$d = substr($dateTime, 0, 2);
+	$m = substr($dateTime, 3, 2);
+	$Y = substr($dateTime, 6, 4);
+	$H = substr($dateTime, 11, 2);
+	$min = substr($dateTime, 14, 2);
+	$dateTime = mktime($H, $min, '00', $m, $d, $Y) + $timeZoneStamp - ( date_default_timezone_set('Europe/Brussels') * 3600 );
+	return $dateTime;
+}
